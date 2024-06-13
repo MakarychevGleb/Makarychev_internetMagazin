d477cec1bb8df4e25df976687af0f85682044a15
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-13 23:00:14 +0300

    first migrations goods

--- InternetMagazin/main/goods/models.py
+++ InternetMagazin/main/goods/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.template.defaultfilters import length
 
-# Create your models here.
+# Create your models{table} here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)

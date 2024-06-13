d354716c1822d29d4310279e3a2de19650a9c81f
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-13 20:04:35 +0300

    goods 1

--- InternetMagazin/main/main/urls.py
+++ InternetMagazin/main/main/urls.py
@@ -21,5 +21,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main2.urls', namespace='main2'))
+    path('', include('main2.urls', namespace='main2')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+# www.site.com/admin/
+# www.site.com
+# www.site.com/about/
+# www.site.com/catalog/
+# www.site.com/catalog/product/

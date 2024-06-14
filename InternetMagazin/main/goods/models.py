07077f83176daf111b647cf687fb9c0f5f4cc418
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-14 18:45:02 +0300

    работа с fixtures в goods

--- InternetMagazin/main/goods/models.py
+++ InternetMagazin/main/goods/models.py
@@ -10,13 +10,16 @@ class Categories(models.Model):
         verbose_name = "Категорию"
         verbose_name_plural = "Категории"
 
+    def __str__(self):
+        return self.name
+
 class Products(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=8, decimal_places=2, verbose_name='Скидка(%)')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка(%)')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Кол-во')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
@@ -24,6 +27,9 @@ class Products(models.Model):
         db_table = "product"
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+    
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
 
 # decimal_places - кол-во знаков после запятой
 # max_digits - кол-во знаков до запятой
--- InternetMagazin/main/main2/views.py
+++ InternetMagazin/main/main2/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
 # Create your views here.
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'ДомСтрой - Главная',
         'content': 'Интернет-магазин мебели "ДомСтрой"',
+        'categories': categories
     }
     return render(request, 'main2/index.html', context)
 

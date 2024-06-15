30b5b238e4cf3e48f8a382c151d62ede57c225cf
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-15 23:52:55 +0300

    синхронизация товаров и фильтров при переходе на след или предыд страницу продуктов

--- InternetMagazin/main/goods/templatetags/goodstags.py
+++ InternetMagazin/main/goods/templatetags/goodstags.py
@@ -1,10 +1,19 @@
 from django import template
+from django.utils.http import urlencode
 
 from goods.models import Categories
 
 register = template.Library()
 
+# для соединение каталога
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
-# для соединение каталога
+# 
+# все конт переменные(takes_context) будут доступны в context во goods/views
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
+#urlencode для создание из словаря ключ значение , в kwargs popadayt imenovannie arg-ti

f93e3d86bcc84c79328bfe1d62e0fdf154ee7463
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-15 22:56:46 +0300

    upgrate пагинации с мет GET

--- InternetMagazin/main/goods/views.py
+++ InternetMagazin/main/goods/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+#
+    page = request.GET.get('page', 1)
 
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -11,7 +13,7 @@ def catalog(request, category_slug, page=1):
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 #количество отоброжаемых товаров на странице (4)
     paginator = Paginator(goods, 4)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': 'ДомСтрой - Каталог',

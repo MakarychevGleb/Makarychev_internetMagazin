2de6daae5df9ea2592559acc54320320bb6b8da5
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-15 21:25:17 +0300

    переhot по id и slug в url строке

--- InternetMagazin/main/goods/views.py
+++ InternetMagazin/main/goods/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-import goods
 from goods.models import Products
 
-# Create your views here.
 def catalog(request):
 
     goods = Products.objects.all()
@@ -14,5 +12,19 @@ def catalog(request):
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+        
+    context = {
+        'product': product
+        
+    }
+
+    return render(request, 'goods/product.html', context=context)
+
+# def product(request, product_slug=False, product_id=False):
+# if product_id:
+# product = Products.objects.get(id=product_id)
+# else:

580db99b504c19a7689379c68e4852dc7e731063
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-16 21:44:49 +0300

    форма с входом login и passsword проблема с reserve

--- InternetMagazin/main/main/settings.py
+++ InternetMagazin/main/main/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+#from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+from django.http import HttpResponseNotFound
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,3 +148,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- InternetMagazin/main/users/views.py
+++ InternetMagazin/main/users/views.py
@@ -1,9 +1,27 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from audioop import reverse
 
+from users.forms import UserLoginForm
+# если данные валидны - верны то аутонтефицируемся перенаправляем
+#  authenticate - есть ли такой пользователь
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main2 index'))
+    else:
+        form = UserLoginForm()
 
     context = {
         'title': 'ДомСтрой - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 

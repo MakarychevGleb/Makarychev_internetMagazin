5ea50ef3a2af4b6672d330ad1a3c5552cc9bc69b
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-16 15:56:23 +0300

    текст поисковика исправен
    при вбиении текста в поисковик того слова что есть в товаре нам он отображается если нет то нет, и footer не весит у текста

--- InternetMagazin/main/goods/utils.py
+++ InternetMagazin/main/goods/utils.py
@@ -1,25 +1,53 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 from goods.models import Products
 
-
 def q_search(query):
-   
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0).order_by("-rank")
+    )
+# для окраска слов из поиска
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name", query,
+            start_sel='<span style="background-color: yellow">',
+            stop_sel="</span>",
+        )
+    )
+    # для окраска слов из поиска
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description", query,
+            start_sel='<span style="background-color: yellow">',
+            stop_sel="</span>",
+        )
+    )
+    return result
 
- # при в воде в поисковик с сортировкой по словам id и slug
-    # keywords = [word for word in query.split() if len(word) > 2]
 
-    # q_objects = Q()
+# при в воде в поисковик с сортировкой по словам id и
+#  при совпадении с товаром на 75% gde rank > 0,
 
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token)
-    #     q_objects |= Q(name__icontains=token)
 
-    # return Products.objects.filter(q_objects)
+# keywords = [word for word in query.split() if len(word) > 2]
+
+# q_objects = Q()
+
+# for token in keywords:
+#     q_objects |= Q(description__icontains=token)
+#     q_objects |= Q(name__icontains=token)
+
+# return Products.objects.filter(q_objects)

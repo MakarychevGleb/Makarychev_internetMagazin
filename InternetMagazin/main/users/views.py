90d107d52c3442553c012a5ea2f70cf21a6291b2
MakarychevGleb <156998553+MakarychevGleb@users.noreply.github.com>
2024-06-17 00:40:55 +0300

    переход по постам и урлкам

--- InternetMagazin/main/users/views.py
+++ InternetMagazin/main/users/views.py
@@ -17,6 +17,9 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, вы успешно зашли в аккаунт")
+# проверка на зарегестрированного
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main2:index'))
     else:
         form = UserLoginForm()
